cb11edabf9e6ed47f4ee6581d5a239030739f1bc
Karol Kądzioła <62353693+karkad96@users.noreply.github.com>
2020-05-30 15:18:35 +0200

    solution to problem 1

--- problem-001/Multiples of 3 and 5.cpp
+++ problem-001/Multiples of 3 and 5.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main()
 {
-	return 0;
+    int sum = 0;
+
+    for (int i = 1; i <= 1000; i++)
+    {
+        if (i % 3 == 0 or i % 5 == 0)
+            sum += i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
 }
