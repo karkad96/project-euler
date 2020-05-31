949411964a38c020a53423848a6f5b0fb49b59b5
Karol Kądzioła <62353693+karkad96@users.noreply.github.com>
2020-05-31 16:48:34 +0200

    solution to problem 18

---
+++ problem-018/Maximum path sum I.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream> 
+#include <algorithm>
+
+using namespace std;
+
+const int N = 15;
+
+int A[N][N];
+
+int main()
+{
+    ifstream file("numbers.txt");
+    string line;
+
+    int line_number = 0;
+
+    while (getline(file, line))
+    {
+        stringstream stream(line);
+        int tmp;
+        int it = 0;
+
+        while (stream >> tmp)
+        {
+            A[line_number][it++] = tmp;
+        }
+
+        line_number++;
+    }
+
+    for (int i = N - 1; i >= 1; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            A[i - 1][j] = max(A[i - 1][j] + A[i][j], A[i - 1][j] + A[i][j + 1]);
+        }
+    }
+
+    cout << A[0][0] << endl;
+
+    return 0;
+}
