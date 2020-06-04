32a5091c90e26a99c98bbc974726d2a76544310f
Karol Kądzioła <62353693+karkad96@users.noreply.github.com>
2020-06-04 15:04:45 +0200

    solution to problem 57

---
+++ problem-057/Square root convergents.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string add(string a, string b)
+{
+    string res = "";
+
+    unsigned int mx = max(a.size(), b.size());
+
+    while (a.size() < mx)
+        a = '0' + a;
+
+    while (b.size() < mx)
+        b = '0' + b;
+
+    int carry = 0;
+
+    for (int i = mx - 1; i >= 0; i--)
+    {
+        int sum = (int)a[i] + (int)b[i] - 96;
+        res = (char)((sum + carry) % 10 + 48) + res;
+        carry = (sum + carry) / 10;
+    }
+
+    if (carry)
+        res = (char)(carry + 48) + res;
+
+    return res;
+
+}
+
+string multiply(string num1, string num2)
+{
+    if (num1.size() == 0 || num2.size() == 0) return "";
+
+    string ret(num1.size() + num2.size() + 1, '0');
+
+    reverse(num1.begin(), num1.end());
+    reverse(num2.begin(), num2.end());
+
+    for (unsigned int i = 0; i < num2.size(); i++)
+    {
+        int dig2 = num2[i] - '0';
+        int carry = 0;
+
+        for (unsigned int j = 0; j < num1.size(); j++)
+        {
+            int dig1 = num1[j] - '0';
+            int temp = ret[i + j] - '0';
+            int cur = dig1 * dig2 + temp + carry;
+            ret[i + j] = cur % 10 + '0';
+            carry = cur / 10;
+        }
+
+        ret[i + num1.size()] = carry + '0';
+    }
+
+    reverse(ret.begin(), ret.end());
+
+    unsigned int pos = ret.find_first_not_of('0', 0);
+
+    if (pos < 0 || pos >= ret.size())
+        pos = ret.size() - 1;
+
+    return ret.substr(pos, ret.size() - pos);
+}
+
+int main()
+{
+    string kn;
+    string hn;
+    string kn_1 = "2";
+    string kn_2 = "1";
+    string hn_1 = "3";
+    string hn_2 = "1";
+    string an = "2";
+
+    int count = 0;
+
+    for (int i = 2; i <= 1000; i++)
+    {
+        hn = add(multiply(an, hn_1), hn_2);
+        kn = add(multiply(an, kn_1), kn_2);
+
+        kn_2 = kn_1;
+        hn_2 = hn_1;
+        hn_1 = hn;
+        kn_1 = kn;
+
+        if (hn.size() > kn.size())
+            count++;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
